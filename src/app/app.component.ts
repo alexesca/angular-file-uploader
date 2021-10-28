c3f391f698f487e4dc6e3cd4c98b6a4f9d0a6bbd
alexesca <alexescamore@gmail.com>
2021-10-28 12:20:16 -0600

    Added code to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {HttpClient, HttpEventType} from '@angular/common/http';
+import {Subscription} from 'rxjs';
+import {finalize} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'file-uploader-example';
+  @Input()
+  requiredFileType: string;
+
+  fileName = '';
+  uploadProgress: number;
+  uploadSub: Subscription;
+
+  constructor(private http: HttpClient) {}
+
+  onFileSelected(event): any {
+    const file: File = event.target.files[0];
+
+    if (file) {
+      this.fileName = file.name;
+      const formData = new FormData();
+      formData.append('thumbnail', file);
+
+      const upload$ = this.http.post('/api/thumbnail-upload', formData, {
+        reportProgress: true,
+        observe: 'events'
+      })
+        .pipe(
+          finalize(() => this.reset())
+        );
+
+      this.uploadSub = upload$.subscribe((event: any) => {
+        if (event.type === HttpEventType.UploadProgress) {
+          this.uploadProgress = Math.round(100 * (event.loaded / event.total));
+        }
+      });
+    }
+  }
+
+  cancelUpload(): void {
+    this.uploadSub.unsubscribe();
+    this.reset();
+  }
+
+  reset(): void {
+    this.uploadProgress = null;
+    this.uploadSub = null;
+  }
 }
